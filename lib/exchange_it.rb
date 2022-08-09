e9c552243024c2482fd1484730880b9ab189d995
MiBBy-4 <maksim.762373maks@bk.ru>
2022-08-09 13:57:43 +0300

    feat: implement account for users

--- lib/exchange_it.rb
+++ lib/exchange_it.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 # Main file where we will require all files and start them
+require 'forwardable'
+require_relative 'exchange_it/utils/uid'
 require_relative 'exchange_it/user'
+require_relative 'exchange_it/account'
+
