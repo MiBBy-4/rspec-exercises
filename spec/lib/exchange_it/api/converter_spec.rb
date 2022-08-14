dd1b267f0278560cf946f6a96802fd27aed97113
MiBBy-4 <maksim.762373maks@bk.ru>
2022-08-14 16:46:52 +0300

    feat: implement transfer with conversion method in account and cover it with mocked tests

--- spec/lib/exchange_it/api/converter_spec.rb
+++ spec/lib/exchange_it/api/converter_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe ExchangeIt::Api::Converter do
 
     # The same way to allow method in Stub
     allow(converter_stub).to receive(:convert).with(sum: 80).and_return(100)
-  
+
     expect(converter_stub.convert(sum: 80)).to eq(100)
 
     # Spy
-    expect(converter_stub).to have_receive(:convert).with(sum: 80).once
+    expect(converter_stub).to have_received(:convert).with(sum: 80).once
   end
 end
